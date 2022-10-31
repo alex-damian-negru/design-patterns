ef58c65ee1436acc96c6ddebd0ae0a71db7a912f
Alex Negru <a.negru@sweatco.in>
2022-10-31 12:27:07 +0200

    Replace YARD with RBS for `factory.rb`

--- creational/factory.rb
+++ creational/factory.rb
@@ -7,42 +7,35 @@
 module Notifications
   module Entities
     class Notification
-      # @raise [NotImplementedError]
       def content = raise NotImplementedError
     end
 
     class Push < Notification
-      # @return [String]
       def content = "This is a Push Notification.\n"
     end
 
     class Email < Notification
-      # @return [String]
       def content = "This is an Email Notification.\n"
     end
   end
 
   module Creators
     class Creator
-      # @raise [NotImplementedError]
       def notification = raise NotImplementedError
 
       def submit = puts notification.content
     end
 
     class Push < Creator
-      # @return [Notifications::Entities::Push]
       def notification = Entities::Push.new
     end
 
     class Email < Creator
-      # @return [Notifications::Entities::Email]
       def notification = Entities::Email.new
     end
   end
 end
 
-# @param creator [Notifications::Creators::Creator]
 def notify(creator)
   creator.submit
 end
